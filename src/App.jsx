5211ef240d0ec3354f9ad49075a6d15532eb0d15
xianleonromero <leonromeroxian@gmail.com>
2025-10-05 13:46:08 +0200

    nav

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,18 @@ import './App.css'
 function App() {
   return (
     <div className="app">
+      {/* Navbar - Barra de navegación */}
+      <nav className="navbar">
+        <a href="#home" className="logo">Portfolio</a>
+        
+        <ul className="nav-menu">
+          <li><a href="#about">about</a></li>
+          <li><a href="#skills">skills</a></li>
+          <li><a href="#projects">projects</a></li>
+          <li><a href="#contact">contact</a></li>
+        </ul>
+      </nav>
+
       <h1>Mi Portfolio</h1>
     </div>
   )
